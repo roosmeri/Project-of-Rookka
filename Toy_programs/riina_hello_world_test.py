95e7782b3c2908f73d3de9cff60142eb2023826c
Riina <riina.juurmaa@gmail.com>
2017-11-15 23:31:35 +0200

    Closes #3

---
+++ Toy_programs/riina_hello_world_test.py
@@ -0,0 +1,8 @@
+print("Hello world!")
+
+while True:
+    number = int(input("Please enter a number, zero to quit: "))
+    print(number)
+    if number == 0:
+        print("Goodbye!")
+        break
