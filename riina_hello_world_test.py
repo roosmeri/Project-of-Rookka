7aa6e0198aae1ad3f708b5e9cf791ce8168841fd
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2017-11-16 15:42:21 +0200

    poistettu ylimääräinen .py tiedosto juuresta

--- riina_hello_world_test.py
+++
@@ -1,8 +0,0 @@
-print("Hello world!")
-
-while True:
-    number = int(input("Please enter a number, zero to quit: "))
-    print(number)
-    if number == 0:
-        print("Goodbye!")
-        break
