66971b54d076691a540aa0f80696609fe1fb4c95
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2017-11-30 13:02:53 +0200

    objektiluokat modelsseissa ja muutama tietokantakokeilu(turhia)

--- RookkaSite/RookkaProject/models.py
+++ RookkaSite/RookkaProject/models.py
@@ -1,3 +1,27 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Query(models.Model):
+    query_text = models.CharField(max_length=200) #queryObject for solr
+
+    def __str__(self):
+        return self.name
+
+
+class Result(models.Model):
+    query = models.ForeignKey(Query, on_delete=models.CASCADE)
+    result_texts = [] #a collection of articles from solr
+
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    result = models.ForeignKey(Result)
+    article_wantedinfo = "" #what is wanted from the article JSON?
+
+
+    def __str__(self):
+        return self.article_wantedinfo
--- RookkaSite/RookkaSite/settings.py
+++ RookkaSite/RookkaSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'RookkaProject.apps.RookkaprojectConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
