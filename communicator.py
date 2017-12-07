52f4eb5aae03d82a92d74862eaa82496231de3e9
Riikka Raatikainen <riikka.raatikainen@yahoo.com>
2017-12-07 14:32:03 +0200

    toimii myös monen sanan hauille

--- communicator.py
+++ communicator.py
@@ -9,49 +9,33 @@ def process_query(query):
     """
     words = query.split()
     if len(words) == 1:
-        return "(text:" + words[0] + " OR title:" + words[0] + ")"
+        return "(text:" + words[0] + "%20OR%20title:" + words[0] + ")"
     else:
         #(text:(juusto AND pitsa) OR title:(juusto OR pitsa))
         returned = "(text:("
         for word in words[:len(words)-1]:
-            returned = returned + word + " AND "
-        returned += words[len(words)-1] + ") OR title:("
+            returned = returned + word + "%20AND%20"
+        returned += words[len(words)-1] + ")%20OR%20title:("
         for word in words[:len(words)-1]:
-            returned = returned + word + " OR "
+            returned = returned + word + "%20OR%20"
         returned = returned + words[len(words)-1] + "))"
         return returned
 
-query = "juusto pitsa kissa"
-# multiple word queries don't work yet 
+query = "pitsa"
 
 url_start = "http://localhost:8983/solr/finwiki/select?q="
 url_middle = process_query(query)
 url_end = "&wt=python"
 
 url = url_start + url_middle + url_end
+print(url)
 
 connection = urlopen(url)
 response = eval(connection.read())
 
 print(response['response']['numFound'], "documents found.")
 
-# Print the name of each document.
+# Print the name of each document (for some reason prints only 10 names).
 
 for document in response['response']['docs']:
   print("Title =", document['title'])
-
-
-
-##import urllib3, simplejson
-##
-##http = urllib3.PoolManager()
-##response =  http.request('GET','http://localhost:8983/solr/finwiki/select?indent=on&q=juusto&wt=json')
-##print(response.status)
-##json = simplejson.loads(response.data.decode('utf-8'))
-##print(json['response']['numFound'], "documents found.")
-##
-### Print the fields of each document.
-##
-##for document in json['response']['docs']:
-##    for doc_key in document:
-##        print(doc_key, "=", document[doc_key])
