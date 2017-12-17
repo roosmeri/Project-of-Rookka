68f749ccf47b7b6c44f0391d202a4c4a06d9164c
Riikka Raatikainen <riikka.raatikainen@yahoo.com>
2017-12-17 19:05:37 +0200

    #24 pari testiä

---
+++ RookkaSite/RookkaProject/test_result.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from RookkaProject import models
+from RookkaProject import views
+
+class TestResult(TestCase):
+
+    def test_query_word_in_article_list(self):
+        result = models.Result()
+        query_text = "juusto"
+        result.setQueryText(query_text)
+        result = views.query(query_text, result)
+        article_list = result.result_texts[:10]
+
+        for article in article_list:
+            self.assertIn(query_text, (article.text or article.title))
+            #query word should appear in the returned article's text or title
+
+    def test_multiple_query_words_in_article_list(self):
+        result = models.Result()
+        query_text = "avara luonto"
+        result.setQueryText(query_text)
+        result = views.query(query_text, result)
+        article_list = result.result_texts[:10]
+
+        for article in article_list:
+            count = 0
+            for word in query_text.split():
+                if word in (article.text or article.title):
+                    count += 1
+            self.assertGreaterEqual(count, 1)
+            #at least one query word must appear in the returned article's text or title
+
