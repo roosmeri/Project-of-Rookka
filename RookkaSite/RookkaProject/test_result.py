2cd88687eb3004fd6ca49a360f7e86f0d7849433
Roosa Meriläinen <roosa.k.merilainen@helsinki.fi>
2017-12-20 18:04:42 +0200

    tests fixed, works just fine, closes #24

--- RookkaSite/RookkaProject/test_result.py
+++ RookkaSite/RookkaProject/test_result.py
@@ -1,22 +1,23 @@
-from unittest import TestCase
-from RookkaProject import models
+from django.test import TestCase
+
+from RookkaProject.models import Article, Result
 from RookkaProject import views
 
 class TestResult(TestCase):
 
     def test_query_word_in_article_list(self):
-        result = models.Result()
+        result = Result()
         query_text = "juusto"
         result.setQueryText(query_text)
         result = views.query(query_text, result)
         article_list = result.result_texts[:10]
 
         for article in article_list:
-            self.assertIn(query_text, (article.text or article.title))
+            self.assertIn(query_text, (article.text.lower() or article.title.lower()))
             #query word should appear in the returned article's text or title
 
     def test_multiple_query_words_in_article_list(self):
-        result = models.Result()
+        result = Result()
         query_text = "avara luonto"
         result.setQueryText(query_text)
         result = views.query(query_text, result)
@@ -25,7 +26,7 @@ class TestResult(TestCase):
         for article in article_list:
             count = 0
             for word in query_text.split():
-                if word in (article.text or article.title):
+                if word in article.text.lower() or article.title.lower():
                     count += 1
             self.assertGreaterEqual(count, 1)
             #at least one query word must appear in the returned article's text or title
